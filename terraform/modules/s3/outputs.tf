10e69393ad97dbd4c795a1b5112577bde1b92e0f
AarishEjaz <aarishejaz@gmail.com>
2025-06-23 10:09:03 +0530

    added changes in s3 module

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -15,5 +15,5 @@ output "origin_access_identity_path" {
 }
 
 output "regional_domain_name" {
-  value = aws_s3_bucket.website.bucket_domain_name
+  value = aws_s3_bucket.website.bucket_regional_domain_name
 }
